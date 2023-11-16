d0e5493cca43bf6b0126a997ac6224b4980b81d4
SIBO-BALDE <baldesibo@gmail.com>
2023-11-16 10:13:30 +0000

    vide

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
 import { AuthComponent } from './auth/auth.component';
 import {EvaluationComponent} from './evaluation/evaluation.component'
+import { ListeProfesseurComponent } from './liste-professeur/liste-professeur.component';
+import { DetailsProfesseursComponent } from './details-professeurs/details-professeurs.component';
 
 const routes: Routes = [
   {path: 'dashboard', component:DashboardAdminComponent},
   {path: 'auth', component:AuthComponent},
   {path:'evaluation', component:EvaluationComponent},
+  {path:'liste-professeur', component:ListeProfesseurComponent},
+  {path:'detail-professeur', component:DetailsProfesseursComponent},
   {path: '', redirectTo:'auth', pathMatch:'full'},
 ];
 
