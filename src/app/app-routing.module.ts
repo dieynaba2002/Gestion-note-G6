e2f111e4430d920620a82718271904f145fd48c9
dieynaba2002 <dieynabacoly51@gmail.com>
2023-11-17 09:17:18 +0000

    my first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
 import { AuthComponent } from './auth/auth.component';
 import {EvaluationComponent} from './evaluation/evaluation.component'
+import { ListeNoteApprenantComponent } from './liste-note-apprenant/liste-note-apprenant.component';
+import { GestionApprenantComponent } from './gestion-apprenant/gestion-apprenant.component';
 
 const routes: Routes = [
   {path: 'dashboard', component:DashboardAdminComponent},
   {path: 'auth', component:AuthComponent},
-  {path:'evaluation', component:EvaluationComponent},
+  {path:'evaluation', component:EvaluationComponent}, 
+  {path:'liste-note', component:ListeNoteApprenantComponent}, 
+  {path:'gestion-apprenant', component:GestionApprenantComponent}, 
   {path: '', redirectTo:'auth', pathMatch:'full'},
 ];
 
--- src/app/gestion-apprenant/gestion-apprenant.component.ts
+++ src/app/gestion-apprenant/gestion-apprenant.component.ts
@@ -1,10 +1,119 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-gestion-apprenant',
   templateUrl: './gestion-apprenant.component.html',
   styleUrls: ['./gestion-apprenant.component.css']
 })
-export class GestionApprenantComponent {
+export class GestionApprenantComponent implements OnInit{
+
+  apprenants:any[]=[]
+
+  nom:string='';
+  annee:string='';
+  prenom:string='';
+  classe:string='';
+  email:string='';
+  adresse: string = '';
+  dateNaiss: string = '';
+  role:string='';
+  telephone:string='';
+  etat:string="active";
+
+ 
+ idLastApprenant:number=0;
+
+ profRecup:any;
+
+ filterValue:any;
+ textButton:string='';
+ ngOnInit() {
+   if (!localStorage.getItem('apprenants')) {
+     localStorage.setItem('apprenants', JSON.stringify(this.apprenants));
+   }
+   this.profRecup=JSON.parse(localStorage.getItem('apprenants') || '[]')
+   console.log(this.profRecup)
+   
+   if(this.profRecup.length !=0){
+     this.idLastApprenant=this.profRecup[this.profRecup.length-1].idProf
+   }
+   
+
+ }
+
+ // vider champs
+ viderChamps(){
+   this.adresse='';
+   this.nom='';
+   this.annee='';
+   this.telephone='';
+   this.prenom=''; 
+   this.classe=''; 
+   this.email = ''; 
+   this.role = '';
+ }
+
+ // methode pour ajouter profuation
+ ajouterprof(){
+   if(this.adresse=='' || this.nom==''|| this.annee=='' || this.prenom=='' || this.telephone=='' || this.email==''){
+     this.showAlert('Oups', 'veuillez renseigner tous les champs', 'error')
+   }else{
+     
+     let Prof={
+       idProf: this.idLastApprenant +1,
+       adresse:this.adresse,
+       nom:this.nom,
+       annee: this.annee,
+       classe: this.classe,
+       prenom:this.prenom,
+       email:this.email,
+       telephone: this.telephone,
+       dateNaiss: this.dateNaiss,
+       role: this.role,
+       etat:'active'
+     }
+     console.log(Prof);
+     this.profRecup.push(Prof);
+     localStorage.setItem('apprenants', JSON.stringify(this.profRecup))
+     console.log(this.profRecup)
+
+
+   }
+   this.viderChamps()
+ }
+
+
+
+
+
+
+ showAlert(title:any, text:any, icon:any){
+   Swal.fire({
+     title:title,
+     text:text,
+     icon:icon
+   })
+ }
+// profChoisi:any;
+// // recupere l'objet
+// recup(paramProf: any){
+//   this.profChoisi=paramProf;
+
+// }
+// // methode pour changer le texte du bouton
+//  desactiverProf(){
+//   this.profChoisi.etat='desactive'
+//   localStorage.setItem('apprenants', JSON.stringify(this.profRecup))
+//  }
+//  activerProf(){
+//   this.profChoisi.etat='active'
+//   localStorage.setItem('profs', JSON.stringify(this.profRecup))
+//  }
+
+toggleEtat(prof: any) {
+  prof.etat = (prof.etat === 'active') ? 'inactive' : 'active';
+  // Vous pouvez ajouter ici la logique pour mettre à jour l'état du professeur dans votre base de données ou tout autre traitement nécessaire
+}
 
 }
